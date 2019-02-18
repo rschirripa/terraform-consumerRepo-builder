b1a5a90c284873458cb83371ad9bc01642bf98aa
AdamCavaliere <adam.cavaliere@gmail.com>
2019-02-18 08:18:07 -0600

    initial

---
+++ github.tf
@@ -0,0 +1,41 @@
+
+data "template_file" "create" {
+  template = "${file("create_repo.tmpl")}"
+  vars = {
+    git_token = "${var.git_token}"
+    new_git_repo = "${var.new_git_repo}"
+    git_user = "${var.git_user}"
+    json_file = "${data.template_file.vcs.rendered}"
+  }
+}
+
+data "template_file" "vcs" {
+  template = "${file(vcs_json.tmpl")}"
+  vars = {
+    reference_git_repo = "${var.reference_git_repo}"
+  }
+}
+
+
+data "template_file" "delete" {
+  template = "${file("delete_repo.tmpl")}"
+  vars = {
+    git_token = "${var.git_token}"
+    new_git_repo = "${var.new_git_repo}"
+    git_user = "${var.git_user}"
+  }
+}
+
+resource "null_resource" "github_mgmt" {
+  provisioner "local-exec" {
+    command = "${data.template_file.create.rendered}"
+  }
+  
+  provisioner "local-exec"{
+    when = "destroy"
+    command = "${data.template_file.delete.rendered}"
+    
+  }
+  
+}
+
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "git_token" {}
+variable "reference_git_repo" {}
+variable "new_git_repo" {}
+variable "git_user" {}
