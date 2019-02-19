901018bb1184ad801f7512f84a1c49bfaa10a8e9
AdamCavaliere <adam.cavaliere@gmail.com>
2019-02-18 19:46:36 -0600

    Update github.tf

--- github.tf
+++ github.tf
@@ -5,7 +5,6 @@ data "template_file" "create" {
     git_token = "${var.git_token}"
     new_git_repo = "${var.new_git_repo}"
     git_user = "${var.git_user}"
-    json_file = "${data.template_file.vcs.rendered}"
   }
 }
 
